06008d8951d821e6c5e899745cbe54a3c6aea367
slava <skat_113@mail.ru>
2023-11-11 11:03:14 +0300

    стилизация крупных блоков catalog.html

--- source/js/script.js
+++ source/js/script.js
@@ -1,6 +1,9 @@
 let menuButton = document.querySelector(".main-nav__toggle");
 let userNavigation = document.querySelector(".main-nav__wrapper");
 
+
+userNavigation.classList.add("visually-hidden")
+
 menuButton.addEventListener("click", (evt)=>{
   evt.preventDefault;
   userNavigation.classList.toggle("visually-hidden")
