a3e998524a9be13194ea646eccfe31d201131b00
slava <skat_113@mail.ru>
2023-11-13 15:16:27 +0300

    Скрипт слайдера, навигации,добавление стилей

--- source/js/script.js
+++ source/js/script.js
@@ -1,10 +1,32 @@
-let menuButton = document.querySelector(".main-nav__toggle");
-let userNavigation = document.querySelector(".main-nav__wrapper");
-
+const menuButton = document.querySelector(".main-nav__toggle");
+const userNavigation = document.querySelector(".main-nav__wrapper");
+const pageHeader = document.querySelector(".page-header");
 
 userNavigation.classList.add("visually-hidden")
+pageHeader.classList.remove("page-header--opened");
 
-menuButton.addEventListener("click", (evt)=>{
-  evt.preventDefault;
-  userNavigation.classList.toggle("visually-hidden")
-})
+//Конструкция if else
+// menuButton.addEventListener("click", () => {
+//   if (userNavigation.classList.contains("visually-hidden")) {
+//     userNavigation.classList.remove("visually-hidden");
+//     pageHeader.classList.add("page-header--opened");
+//   }
+//   else {
+//     userNavigation.classList.add("visually-hidden");
+//     pageHeader.classList.remove("page-header--opened");
+//   };
+// });
+
+menuButton.addEventListener("click", () => {
+  userNavigation.classList.contains("visually-hidden")
+      ?
+      userNavigation.classList.remove("visually-hidden") &
+      pageHeader.classList.add("page-header--opened") &
+      menuButton.classList.remove("main-nav__toggle--closed") &
+      menuButton.classList.add("main-nav__toggle--opened")
+      :
+      userNavigation.classList.add("visually-hidden") &
+      pageHeader.classList.remove("page-header--opened") &
+      menuButton.classList.remove("main-nav__toggle--opened") &
+      menuButton.classList.add("main-nav__toggle--closed")
+});
